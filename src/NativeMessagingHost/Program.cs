809d7d1864ba5f57dcd568ae15d6d0d02f739d16
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2018-07-24 11:51:07 +0200

    Add the NM host project

---
+++ src/NativeMessagingHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NativeMessagingHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
