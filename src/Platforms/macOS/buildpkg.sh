52c158c41c7473c767420c38b966afb50d8d352a
Morten Frederiksen <mortenfrederiksen@Mortens-MBP.lan>
2018-07-31 14:02:44 +0200

    macOS installer - try something new

--- src/Platforms/macOS/buildpkg.sh
+++ src/Platforms/macOS/buildpkg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pkgbuild --root bundle --identifier com.github.frederiksen.PlatformBridgeDemo --version 0.1 bookmarks.pkg
+pkgbuild --root ROOT --identifier com.github.frederiksen.chromeplatformbridge --version 0.1 bookmarks.pkg
 hdiutil create -volname platform-bridge-demo -srcfolder ./bookmarks.pkg -ov -format UDZO platform-bridge-demo-macos.dmg
