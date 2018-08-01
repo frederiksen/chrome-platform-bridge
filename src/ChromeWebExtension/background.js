041d20d7853d977ae8a4f87c6c03f2bb5a610c4b
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2018-08-01 18:26:04 +0200

    Cleanup and bump version number

--- src/ChromeWebExtension/background.js
+++ src/ChromeWebExtension/background.js
@@ -1,6 +1,29 @@
+/*
+MIT License
+
+Copyright (c) 2018 Morten Frederiksen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 (function () {
   // Native messages port
-  var port = null;
+  let port=null;
   // Message from native app
   function onNativeMessageReceived(message) {
     sendMessageToContentScript(message.message);
@@ -11,14 +34,13 @@
   }
   // Native app was disconnected
   function onDisconnected() {
-    port = null;
+    port=null;
   }
   // Messages from the content-script
   chrome.runtime.onMessage.addListener(function (request, sender) {
     // Try to connect to the native app, if not already connected
-    if (port == null) {
-      var hostName = "com.github.frederiksen.chromeplatformbridge";
-      port = chrome.runtime.connectNative(hostName);
+    if (port===null) {
+      port=chrome.runtime.connectNative("com.github.frederiksen.chromeplatformbridge");
       port.onMessage.addListener(onNativeMessageReceived);
       port.onDisconnect.addListener(onDisconnected);
     }
