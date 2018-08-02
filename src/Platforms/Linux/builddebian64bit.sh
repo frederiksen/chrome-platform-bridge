87331901476d17cf6de19a76069826236498b80a
frederiksen <mrtn.frederiksen@gmail.com>
2018-08-02 19:36:23 +0200

    Improve bash script for creating the Linux .deb package

--- src/Platforms/Linux/builddebian64bit.sh
+++ src/Platforms/Linux/builddebian64bit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+mkdir -p platform-bridge-demo-linux64bit/DEBIAN
+cp control ./platform-bridge-demo-linux64bit/DEBIAN
+mkdir -p platform-bridge-demo-linux64bit/etc/opt/chrome/native-messaging-hosts
+cp com.github.frederiksen.chromeplatformbridge.json ./platform-bridge-demo-linux64bit/etc/opt/chrome/native-messaging-hosts
 mkdir -p platform-bridge-demo-linux64bit/usr/lib/platform-bridge-demo
 cp ../../NativeMessagingHost/bin/Release/netcoreapp2.1/linux-x64/publish/* platform-bridge-demo-linux64bit/usr/lib/platform-bridge-demo
+# create .deb package
 dpkg --build platform-bridge-demo-linux64bit
 # cleanup
-rm -rf platform-bridge-demo-linux64bit/usr/lib/platform-bridge-demo
+rm -rf platform-bridge-demo-linux64bit
