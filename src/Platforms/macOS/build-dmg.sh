caddaea00cc72d2312287ce15ea81b36defbfebc
Morten Frederiksen <mortenfrederiksen@Mortens-MBP.lan>
2018-08-02 10:21:09 +0200

    Fully functional macOS installer

---
+++ src/Platforms/macOS/build-dmg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p ROOT/Library/Google/Chrome/NativeMessagingHosts
+cp ./com.github.frederiksen.chromeplatformbridge.json ./ROOT/Library/Google/Chrome/NativeMessagingHosts
+mkdir -p ROOT/Library/chrome-platform-bridge-demo
+cp ../../NativeMessagingHost/bin/Release/netcoreapp2.1/osx-x64/publish/* ROOT/Library/chrome-platform-bridge-demo
+chmod +x ROOT/Library/chrome-platform-bridge-demo/NativeMessagingHost
+
+pkgbuild --root ROOT --identifier com.myfunkyapps.chromeplatformbridge --version 0.3 chrome-platform-bridge.unsigned.pkg
+productsign --sign "Developer ID Installer: MyFunkyApps.com" chrome-platform-bridge.unsigned.pkg chrome-platform-bridge.pkg
+hdiutil create -volname platform-bridge-demo -srcfolder ./chrome-platform-bridge.pkg -ov -format UDZO platform-bridge-demo-macos.dmg
+
+# cleanup
+rm -rf ROOT
--- src/Platforms/macOS/buildpkg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pkgbuild --root ROOT --identifier com.github.frederiksen.chromeplatformbridge --version 0.1 bookmarks.pkg
-hdiutil create -volname platform-bridge-demo -srcfolder ./bookmarks.pkg -ov -format UDZO platform-bridge-demo-macos.dmg
