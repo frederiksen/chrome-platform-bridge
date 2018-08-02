4465ed29ffa31c4fc52c5bbb947474fa28ca73c3
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2018-08-02 20:06:23 +0200

    Add comments

--- src/Platforms/macOS/build-dmg.sh
+++ src/Platforms/macOS/build-dmg.sh
@@ -4,10 +4,9 @@ cp ./com.github.frederiksen.chromeplatformbridge.json ./ROOT/Library/Google/Chro
 mkdir -p ROOT/Library/chrome-platform-bridge-demo
 cp ../../NativeMessagingHost/bin/Release/netcoreapp2.1/osx-x64/publish/* ROOT/Library/chrome-platform-bridge-demo
 chmod +x ROOT/Library/chrome-platform-bridge-demo/NativeMessagingHost
-
+# create the pkg then sign it then put it in a dmg
 pkgbuild --root ROOT --identifier com.myfunkyapps.chromeplatformbridge --version 0.3 chrome-platform-bridge.unsigned.pkg
 productsign --sign "Developer ID Installer: MyFunkyApps.com" chrome-platform-bridge.unsigned.pkg chrome-platform-bridge.pkg
 hdiutil create -volname platform-bridge-demo -srcfolder ./chrome-platform-bridge.pkg -ov -format UDZO platform-bridge-demo-macos.dmg
-
 # cleanup
 rm -rf ROOT
