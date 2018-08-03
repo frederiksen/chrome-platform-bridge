8c0760cbd0c542bdac42315d4d88871f9d606f60
frederiksen <mrtn.frederiksen@gmail.com>
2018-08-03 10:28:57 +0200

    Cleanup and bump version

--- src/Platforms/Linux/builddebian64bit.sh
+++ src/Platforms/Linux/builddebian64bit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir -p platform-bridge-demo-linux64bit/DEBIAN
-cp control ./platform-bridge-demo-linux64bit/DEBIAN
-mkdir -p platform-bridge-demo-linux64bit/etc/opt/chrome/native-messaging-hosts
-cp com.github.frederiksen.chromeplatformbridge.json ./platform-bridge-demo-linux64bit/etc/opt/chrome/native-messaging-hosts
-mkdir -p platform-bridge-demo-linux64bit/usr/lib/platform-bridge-demo
-cp ../../NativeMessagingHost/bin/Release/netcoreapp2.1/linux-x64/publish/* platform-bridge-demo-linux64bit/usr/lib/platform-bridge-demo
+mkdir -p platform-bridge-demo-linux/DEBIAN
+cp control ./platform-bridge-demo-linux/DEBIAN
+mkdir -p platform-bridge-demo-linux/etc/opt/chrome/native-messaging-hosts
+cp com.github.frederiksen.chromeplatformbridge.json ./platform-bridge-demo-linux/etc/opt/chrome/native-messaging-hosts
+mkdir -p platform-bridge-demo-linux/usr/lib/platform-bridge-demo
+cp ../../NativeMessagingHost/bin/Release/netcoreapp2.1/linux-x64/publish/* platform-bridge-demo-linux/usr/lib/platform-bridge-demo
 # create .deb package
-dpkg --build platform-bridge-demo-linux64bit
+dpkg --build platform-bridge-demo-linux
 # cleanup
-rm -rf platform-bridge-demo-linux64bit
+rm -rf platform-bridge-demo-linux
