f18446b1313c8851e8af214417758e43c0213a16
Morten Frederiksen <morten@myfunkyapps.com>
2020-01-29 18:59:08 +0100

    .NET Core 3.1

--- src/Platforms/Linux/builddebian64bit.sh
+++ src/Platforms/Linux/builddebian64bit.sh
@@ -4,7 +4,7 @@ cp control ./platform-bridge-demo-linux/DEBIAN
 mkdir -p platform-bridge-demo-linux/etc/opt/chrome/native-messaging-hosts
 cp com.github.frederiksen.chromeplatformbridge.json ./platform-bridge-demo-linux/etc/opt/chrome/native-messaging-hosts
 mkdir -p platform-bridge-demo-linux/usr/lib/platform-bridge-demo
-cp ../../NativeMessagingHost/bin/Release/netcoreapp2.1/linux-x64/publish/* platform-bridge-demo-linux/usr/lib/platform-bridge-demo
+cp ../../NativeMessagingHost/bin/Release/netcoreapp3.1/linux-x64/publish/* platform-bridge-demo-linux/usr/lib/platform-bridge-demo
 # create .deb package
 dpkg --build platform-bridge-demo-linux
 # cleanup
