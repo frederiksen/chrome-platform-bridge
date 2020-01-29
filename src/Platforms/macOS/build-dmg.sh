bb52c62b9faea2c458942211fb762f4ea76fbe82
Morten Frederiksen <morten@myfunkyapps.com>
2020-01-29 18:56:52 +0100

    .NET Core 3.1

--- src/Platforms/macOS/build-dmg.sh
+++ src/Platforms/macOS/build-dmg.sh
@@ -2,7 +2,7 @@
 mkdir -p ROOT/Library/Google/Chrome/NativeMessagingHosts
 cp ./com.github.frederiksen.chromeplatformbridge.json ./ROOT/Library/Google/Chrome/NativeMessagingHosts
 mkdir -p ROOT/Library/chrome-platform-bridge-demo
-cp ../../NativeMessagingHost/bin/Release/netcoreapp2.1/osx-x64/publish/* ROOT/Library/chrome-platform-bridge-demo
+cp ../../NativeMessagingHost/bin/Release/netcoreapp3.1/osx-x64/publish/* ROOT/Library/chrome-platform-bridge-demo
 chmod +x ROOT/Library/chrome-platform-bridge-demo/NativeMessagingHost
 # create the pkg then sign it then put it in a dmg
 pkgbuild --root ROOT --identifier com.myfunkyapps.chromeplatformbridge --version 1.0 chrome-platform-bridge.unsigned.pkg
